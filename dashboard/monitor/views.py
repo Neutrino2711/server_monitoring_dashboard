54fe42fa3249e638680c8388dc2eefd326f4b7cc
Neutrino2711 <akshajpal24@gmail.com>
2025-04-26 22:26:15 +0530

    monitor-views-added

--- dashboard/monitor/views.py
+++ dashboard/monitor/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.core.cache import cache
+from rest_framework import generics,status 
+from rest_framework.response import Response 
+from rest_framework.throttling import UserRateThrottle 
+from .models import Server, ServerMetric 
+from .serializers import ServerSerializer, ServerMetricSerializer
 
-# Create your views here.
+class CustomThrottle(UserRateThrottle):
+    rate = '100/hour'
+
+class ServerListView(generics.ListCreateAPIView):
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+    throttle_classes =[CustomThrottle]
+
+class ServerDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+    throttle_classes = [CustomThrottle]
+
+class ServerMetricsView(generics.ListAPIView):
+    serializer_class = ServerMetricSerializer
+    throttle_classes = [CustomThrottle]
+
+    def get_queryset(self):
+        server_id = self.kwargs['server_id']
+        return ServerMetric.objects.filter(server_id= server_id).order_by('-timestamp')[:100]
+    
+    def list(self,request,*args,**kwargs):
+        cache_key = f"server_{self.kwargs['server_id']}_metrics"
+        cached_data = cache.get(cache_key)
+
+        if cached_data:
+            return Response(cached_data)
+        
+        # If not 
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset,many = True)
+
+        # Cache the response for 1 minute 
+        cache.set(cache_key,serializer.data, timeout=60)
+
+        return Response(serializer.data)
+    
+    
